66a9f3313dce617ed475bbd2cc572a7d77845692
babobski <info@babobski.nl>
2016-05-24 08:05:08 +0200

    updated version numbers

--- content/FileScopes.js
+++ content/FileScopes.js
@@ -1,6 +1,6 @@
 if (typeof(extensions) === 'undefined') extensions = {};
 if (typeof(extensions.less) === 'undefined') extensions.less = {
-	version: '2.1.0'
+	version: '2.1.1'
 };
 
 (function() {
--- content/overlay.js
+++ content/overlay.js
@@ -5,7 +5,7 @@ xtk.load('chrome://less/content/less/less.min.js');
  */
 if (typeof(extensions) === 'undefined') extensions = {};
 if (typeof(extensions.less) === 'undefined') extensions.less = {
-	version: '2.1.0'
+	version: '2.1.1'
 };
 (function() {
 	var notify = require("notify/notify"),
